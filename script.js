690e7b03ee5a6fc20ba2a77a0951b964ab888a15
Fernando Santillan <ing.fernando.santillan@gmail.com>
2023-02-22 23:24:56 -0600

    Carga de archivos del proyectos

---
+++ script.js
@@ -0,0 +1,15 @@
+const checkboxes = document.getElementsByClassName("checkbox");
+
+if (checkboxes[x].checked) {
+    window.alert('Bien')  
+ }
+
+
+/* 
+if (checkboxes[x].checked) {
+    cont = cont + 1;
+   }
+
+if (cont ==4){
+    console.log("Ganaste")
+} */
